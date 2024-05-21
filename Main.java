4ac9671d29bf384b69696f01c65d74bd52a4d244
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-05-21 14:32:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 
 public class Main {
 	public static void main(String[] args) {
-		final String path = "C:\\Users\\alexb\\eclipse-workspace\\Chat\\src\\Msg.txt";
-		readMsg msg = new readMsg();
-		Ui ui = new Ui();
+
+		MediadorConcreto mediador = new MediadorConcreto();
 		
-		ui.window();
+		Usuario usuario1 = new Usuario("Alex");
+		usuario1.setMediador(mediador);
+		Ui ui1 = new Ui(usuario1);
+		mediador.setUser1(usuario1);
+		
+		Usuario usuario2 = new Usuario("Marco");
+		usuario2.setMediador(mediador);
+		Ui ui2 = new Ui(usuario2);
+		mediador.setUser2(usuario2);
+		
+		ui1 = new Ui(usuario1);
+		ui2 = new Ui(usuario2);
+		
+		ui1.window();
+		ui2.window();
 	}
 }
