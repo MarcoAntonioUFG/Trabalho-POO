6cdf359a9373701eb2e019d404443f819a9a41f4
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-05-20 22:45:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		final String path = "C:\\Users\\alexb\\eclipse-workspace\\Chat\\src\\Msg.txt";
+		readMsg msg = new readMsg();
+		Ui ui = new Ui();
+		
+		ui.window();
+	}
 }
