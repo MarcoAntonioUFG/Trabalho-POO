8620901e20f5a3371a7a72dbd49a46d5a99d1dbc
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-05-28 23:17:39 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,14 @@
 
 public class Main {
 	public static void main(String[] args) {
-
+	
 		MediadorConcreto mediador = new MediadorConcreto();
 		
-		Usuario usuario1 = new Usuario("Alex");
-		usuario1.setMediador(mediador);
+		Usuario usuario1 = new Usuario("Alex", mediador);
+		Usuario usuario2 = new Usuario("Marco", mediador);
+		
 		Ui ui1 = new Ui(usuario1);
-		mediador.setUser1(usuario1);
-		
-		Usuario usuario2 = new Usuario("Marco");
-		usuario2.setMediador(mediador);
 		Ui ui2 = new Ui(usuario2);
-		mediador.setUser2(usuario2);
 		
-		ui1 = new Ui(usuario1);
-		ui2 = new Ui(usuario2);
-		
-		ui1.window();
-		ui2.window();
 	}
 }
