7a9720cf225e8fbfbfd5e891026f8476b32adb76
MarcoAntonioUFG <146884614+MarcoAntonioUFG@users.noreply.github.com>
2024-06-19 15:24:09 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
 	
 		MediadorConcreto mediador = new MediadorConcreto();
 		
-		Usuario usuario1 = new Usuario("Alex", mediador);
-		Usuario usuario2 = new Usuario("Marco", mediador);
+		String nome1 = JOptionPane.showInputDialog("Insira seu nome:");
+		String nome2 = JOptionPane.showInputDialog("Insira seu nome:");
+		
+		
+		Usuario usuario1 = new Usuario(nome1, mediador);
+		Usuario usuario2 = new Usuario(nome2, mediador);
 		
 		Ui ui1 = new Ui(usuario1);
 		Ui ui2 = new Ui(usuario2);
