327e1d1fdfd389c6f84e836c23b50df5420fa66b
MarcoAntonioUFG <146884614+MarcoAntonioUFG@users.noreply.github.com>
2024-06-25 16:51:39 -0300

    Update Mediador.java

--- Mediador.java
+++ Mediador.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public interface Mediador {
-	void Notificar(Usuario user);
 	void addMessage(Usuario remetente, String mensagem);
 	ArrayList<String> getHistorico();
 	void addUsuario(Usuario usuario);
